e280b607e11e5477a7a8a1540ebfdb293d6bced8
merfew <s_dobrynina.04@mail.ru>
2023-11-10 15:15:26 +0300

    First commit

---
+++ ооп лаба 4/ооп лаба 4/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+
+class Animal
+{
+    private string kind = "Кот";
+    private int age = 6;
+    public string Name { get; set; } = "Мурзик";
+
+    public string Kind
+    {
+        get
+        {
+            return kind;
+        }
+        set
+        {
+            kind = value;
+        }
+    }
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if(age > 0)
+            {
+                age = value;
+            }
+        }
+    }
+
+    public bool Check
+    {
+        get
+        {
+            if( age < 25)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public string GetInfo()
+    {
+        return $"Имя: {Name}, Вид: {Kind}, Возраст: {Age}, Проверка: {Check}";
+    }
+
+    public string GetInfo (bool includeIntProperty)
+    {
+        if (includeIntProperty == true)
+        {
+            return GetInfo();
+        }
+        else
+        {
+            return $"Имя: {Name}, Вид: {Kind}, Проверка: {Check}";
+        }
+    }
+}
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Console.WriteLine(animal.GetInfo());
+            Console.WriteLine(animal.GetInfo(false));
+        }
+    }
+}
